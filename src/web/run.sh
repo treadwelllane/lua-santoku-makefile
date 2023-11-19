b12a1042c98ce0d0aa6b869b9bbb4d673f85c529
Matthew Brooks <mb964033@gmail.com>
2023-11-19 19:41:46 +0000

    clean up logs

--- src/web/run.sh
+++ src/web/run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 <%
-  gen = require("santoku.gen")
   str = require("santoku.string")
   vec = require("santoku.vector")
   server = server or {}
@@ -10,8 +9,21 @@
 set -ex
 cd "$(dirname $0)"
 
-<% return vec.wrap(server.run_envs or {}):extend(str.split(os.getenv("RUN_ENVS") or "")):map(function (env)
-  return ". " .. env
-end):concat("\n") %>
+<% return vec.wrap(server.run_envs or {}):extend(str.split(os.getenv("RUN_ENVS") or ""))
+  :filter(function (env)
+    return not str.isempty(env)
+  end)
+  :map(function (env)
+    return ". " .. env
+  end):concat("\n") %>
 
-openresty -c "nginx.conf" -p "$PWD" -e error.log
+mkdir -p logs
+touch logs/error.log logs/access.log
+
+openresty -p "$PWD" -c nginx.conf
+
+if [ "$<% return variable_prefix %>_FG" = "1" ]
+then
+  trap "kill \"$(cat server.pid)\"" EXIT
+  tail -qf logs/*
+fi
